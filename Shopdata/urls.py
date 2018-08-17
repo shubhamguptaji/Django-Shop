290b0d01e845f125e76bd1b77ad528cfa9040dae
Shubham Gupta <shubham2604gupta@gmail.com>
2018-08-17 22:54:27 +0530

    create new category added

--- Shopdata/urls.py
+++ Shopdata/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.categories, name='categories'),
+    path('categories/new/', views.newCategory, name='newCategory'),
     path('categories/<int:Categories_id>/item/', views.showItem, name='ShowItem'),
     path('categories/<int:Categories_id>/edit/', views.editCategory, name='editCategory'),
     path('categories/<int:Categories_id>/delete/', views.deleteCategory, name='deleteCategory'),
-    path('categories/<int:Categories_id>/item/<int:Items_id>/delete', views.deleteItem, name='deleteItem'),
-    path('categories/<int:Categories_id>/item/<int:Items_id>/edit', views.editItem, name='editItem'),
-    path('categories/<int:Categories_id>/item/new', views.newItem, name='newItem'),
+    path('categories/<int:Categories_id>/item/<int:Items_id>/delete/', views.deleteItem, name='deleteItem'),
+    path('categories/<int:Categories_id>/item/<int:Items_id>/edit/', views.editItem, name='editItem'),
+    path('categories/<int:Categories_id>/item/new/', views.newItem, name='newItem'),
 ]
--- Shopdata/views.py
+++ Shopdata/views.py
@@ -5,10 +5,11 @@ from .models import Categories, Items
 
 # Create your views here.
 
+
 def categories(request):
     category = Categories.objects.all()
     categories = {
-        'categories' : category
+        'categories': category
     }
     return render(request, 'Shopdata/Categories.html', categories)
 
@@ -17,49 +18,81 @@ def showItem(request, Categories_id):
     category = get_object_or_404(Categories, pk=Categories_id)
     items = Items.objects.all()
     data = {
-        'category' : category.cName,
-        'items' : items
+        'category': category,
+        'items': items,
     }
+    print(items[1].categories_id)
     return render(request, 'Shopdata/showItem.html', data)
 
 
-def editCategory(request,Categories_id):
+def editCategory(request, Categories_id):
     category = get_object_or_404(Categories, pk=Categories_id)
     categories = {
-        'category' : category.cName
+        'category': category.cName
     }
-    return render(request, 'Shopdata/editCategory.html', categories)
+    if request.method == 'POST':
+        # if request.get('name') and request.get('image'):
+        editedCategory = Categories()
+        editedCategory.cName = request.get('name')
+        editedCategory.image = request.get('image')
+        editedCategory.save()
+        print("done")
+        return render(request, 'Shopdata/Categories.html', categories)
+    else:
+        print("Not done")
+        return render(request, 'Shopdata/editCategory.html', categories)
+
 
 def deleteCategory(request, Categories_id):
+    allCategories = Categories.objects.all()
     category = get_object_or_404(Categories, pk=Categories_id)
     categories = {
-        'category' : category.cName
+        'category': category.cName
     }
-    return render(request, 'Shopdata/deleteCategory.html', categories)
+    if request.method == 'POST':
+        category.delete()
+        print("Done")
+        return render(request, 'Shopdata/Categories.html', allCategories)
+    else:
+        return render(request, 'Shopdata/deleteCategory.html', categories)
 
 
 def newItem(request, Categories_id):
     category = get_object_or_404(Categories, pk=Categories_id)
     data = {
-        'category' : category.cName
+        'category': category.cName
     }
     return render(request, 'Shopdata/newItem.html', data)
 
 
 def deleteItem(request, Categories_id, Items_id):
-    category = get_object_or_404(Categories, pk =Categories_id)
+    category = get_object_or_404(Categories, pk=Categories_id)
     item = get_object_or_404(Items, pk=Items_id)
     data = {
-        'category' : category.cName,
-        'item' : item.name
+        'category': category,
+        'item': item
     }
     return render(request, 'Shopdata/deleteItem.html', data)
 
+
 def editItem(request, Categories_id, Items_id):
-    category = get_object_or_404(Categories, pk =Categories_id)
+    category = get_object_or_404(Categories, pk=Categories_id)
     item = get_object_or_404(Items, pk=Items_id)
     data = {
-        'category' : category.cName,
-        'item' : item.name
+        'category': category,
+        'item': item.name,
     }
     return render(request, 'Shopdata/editItem.html', data)
+
+
+def newCategory(request):
+    if request.method == 'POST':
+        # if request.get('name') and request.get('image'):
+        editedCategory = Categories()
+        editedCategory.cName = request.POST.get('name')
+        editedCategory.image = request.POST.get('image')
+        editedCategory.save()
+        print("done")
+        return render(request, 'Shopdata/newCategory.html')
+    else:
+        return render(request, 'Shopdata/newCategory.html')
